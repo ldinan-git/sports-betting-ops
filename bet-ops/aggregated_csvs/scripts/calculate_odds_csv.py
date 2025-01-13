c65e8c34ab7c2c7f4f71f77f344b68687155ece2
= <ldinan@captechventures.com>
2025-01-13 09:06:44 -0500

    Automated commit for basketball_nba on 20250113

--- bet-ops/aggregated_csvs/scripts/calculate_odds_csv.py
+++ bet-ops/aggregated_csvs/scripts/calculate_odds_csv.py
@@ -32,11 +32,11 @@ def calculate_dejuice(row, new_df):
         dejuice = row[bookmaker] / denom
         dejuice_values[bookmaker.replace('_implied_prob', '_dejuice')] = dejuice
         if dejuice == 1:
-            if row[bookmaker] < 0.7:
+            if row[bookmaker] > 0.3:
                 denom = 1.05
-            elif row[bookmaker] < 0.8:
+            elif row[bookmaker] > 0.2:
                 denom = 1.1
-            elif row[bookmaker] < 0.9:
+            elif row[bookmaker] > 0.1:
                 denom = 1.15
             else:
                 denom = 1.2
